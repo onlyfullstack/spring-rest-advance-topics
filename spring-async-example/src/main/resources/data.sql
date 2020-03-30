7b783eccabb1c3bf1509c535e54ec9902037fa69
ssoza1 <saurabh.s.oza@aexp.com>
2020-03-30 21:25:44 +0100

    Updating the scripts

--- spring-async-example/src/main/resources/data.sql
+++ spring-async-example/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO employee_data(id, first_name, last_name, salary, email) VALUES (1, 'saurabh', 'oza', 1000, 'saurabh @abc.com');
-INSERT INTO employee_data(id, first_name, last_name, salary, email) VALUES (2, 'only', 'fullstack', 2000, 'only@abc.com');
+INSERT INTO employee_data(id, first_name, last_name, salary, email) VALUES (1, 'saurabh', 'oza', 1000, 'saurabh@abc.com');
+INSERT INTO employee_data(id, first_name, last_name, salary, email) VALUES (2, 'only', 'fullstack', 2000, 'onlyfullstack@abc.com');
 INSERT INTO employee_data(id, first_name, last_name, salary, email) VALUES (3, 'abc', 'xyz', 1000, 'abc@abc.com');
